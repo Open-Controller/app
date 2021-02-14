6f7c1b0506725c1182558d0583cd0a7e0ef21457
PJTSearch <pjtsignups@gmail.com>
2021-02-13 16:11:36 -0800

    refactor: Remove old opencontroller-lib

--- app/src/main/java/com/pjtsearch/opencontroller/ui/components/ControllerView.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/ui/components/ControllerView.kt
@@ -1,7 +1,6 @@
 package com.pjtsearch.opencontroller.ui.components
 
 import androidx.compose.runtime.Composable
-import com.pjtsearch.opencontroller_lib.OpenController
 import com.pjtsearch.opencontroller_lib_proto.ControllerOrBuilder
 
 @Composable
