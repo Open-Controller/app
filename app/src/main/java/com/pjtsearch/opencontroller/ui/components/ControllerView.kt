0efe3b79f8f79912490965efbcc72decbc56a9b6
PJTSearch <pjtsignups@gmail.com>
2021-02-15 11:30:10 -0800

    feat(ControllerView): Add max space between Widgets

--- app/src/main/java/com/pjtsearch/opencontroller/ui/components/ControllerView.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/ui/components/ControllerView.kt
@@ -2,6 +2,7 @@ package com.pjtsearch.opencontroller.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -11,7 +12,7 @@ import com.pjtsearch.opencontroller_lib_proto.ControllerOrBuilder
 
 @Composable
 fun ControllerView(controller: ControllerOrBuilder, executor: OpenControllerLibExecutor) =
-    Column(Modifier.fillMaxWidth()) {
+    Column(Modifier.fillMaxSize(), Arrangement.SpaceBetween) {
         controller.widgetList.map {
             Widget(it, executor, Modifier.fillMaxWidth())
         }
