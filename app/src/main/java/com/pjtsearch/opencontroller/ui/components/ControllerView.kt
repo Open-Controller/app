b11ec92c640d95ce70c167628057311c784f1a86
PJTSearch <pjtsignups@gmail.com>
2021-02-15 12:33:09 -0800

    feat(ControllerView): Change to constant space btw sections

--- app/src/main/java/com/pjtsearch/opencontroller/ui/components/ControllerView.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/ui/components/ControllerView.kt
@@ -1,19 +1,17 @@
 package com.pjtsearch.opencontroller.ui.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.pjtsearch.opencontroller_lib_android.OpenControllerLibExecutor
 import com.pjtsearch.opencontroller_lib_proto.ControllerOrBuilder
 
 @Composable
 fun ControllerView(controller: ControllerOrBuilder, executor: OpenControllerLibExecutor) =
-    Column(Modifier.fillMaxSize(), Arrangement.SpaceBetween) {
+    Column(Modifier.fillMaxSize()) {
         controller.widgetList.map {
-            Widget(it, executor, Modifier.fillMaxWidth())
+            Widget(it, executor, Modifier.fillMaxWidth().padding(bottom = 10.dp))
         }
     }
