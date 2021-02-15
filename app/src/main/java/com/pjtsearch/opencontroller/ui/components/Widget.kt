10ddffbd504e6da529657ad9c2339d630c320205
PJTSearch <pjtsignups@gmail.com>
2021-02-15 11:00:09 -0800

    feat(Widget): Add Row

--- app/src/main/java/com/pjtsearch/opencontroller/ui/components/Widget.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/ui/components/Widget.kt
@@ -1,31 +1,44 @@
 package com.pjtsearch.opencontroller.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import com.pjtsearch.opencontroller_lib_android.OpenControllerLibExecutor
 import com.pjtsearch.opencontroller_lib_proto.WidgetOrBuilder
 import com.pjtsearch.opencontroller_lib_proto.Widget.InnerCase
 import kotlin.concurrent.thread
 
 @Composable
-fun Widget(widget: WidgetOrBuilder, executor: OpenControllerLibExecutor) =
+fun Widget(widget: WidgetOrBuilder, executor: OpenControllerLibExecutor, modifier: Modifier = Modifier) {
     when (widget.innerCase) {
         InnerCase.BUTTON -> Button(
-            onClick = {
-                thread {
-                    executor.executeAction(widget.button.action)
-                }
-            }) {
+                modifier = modifier,
+                onClick = {
+                    thread {
+                        executor.executeAction(widget.button.action)
+                    }
+                }) {
             Text(widget.button.text)
         }
         InnerCase.DYNAMIC_TEXT -> {
             var value: Any? by remember { mutableStateOf(null) }
             DisposableEffect(widget.dynamicText.value) {
-                val unsub = executor.subscribeDynamicValue(widget.dynamicText.value) {value = it}
+                val unsub = executor.subscribeDynamicValue(widget.dynamicText.value) { value = it }
                 onDispose { unsub() }
             }
-            Text(value.toString())
+            Text(value.toString(), modifier)
         }
+        InnerCase.ROW -> Row(modifier.fillMaxWidth(), Arrangement.SpaceBetween) {
+            widget.row.childList.map {
+                Widget(it, executor)
+            }
+        }
+        InnerCase.COLUMN -> TODO()
+        InnerCase.ARROW_LAYOUT -> TODO()
         InnerCase.INNER_NOT_SET -> Text("Widget type must be set")
     }
+}
