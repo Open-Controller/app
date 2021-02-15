d784e88989a28872dbec3b02efa0b0032fd16ec2
PJTSearch <pjtsignups@gmail.com>
2021-02-15 11:14:06 -0800

    feat(ControllerView): Add base Column, align Widgets at center by default

--- app/src/main/java/com/pjtsearch/opencontroller/ui/components/ControllerView.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/ui/components/ControllerView.kt
@@ -1,11 +1,17 @@
 package com.pjtsearch.opencontroller.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.pjtsearch.opencontroller_lib_android.OpenControllerLibExecutor
 import com.pjtsearch.opencontroller_lib_proto.ControllerOrBuilder
 
 @Composable
 fun ControllerView(controller: ControllerOrBuilder, executor: OpenControllerLibExecutor) =
-    controller.widgetList.map {
-        Widget(it, executor)
+    Column(Modifier.fillMaxWidth()) {
+        controller.widgetList.map {
+            Widget(it, executor, Modifier.align(Alignment.CenterHorizontally))
+        }
     }
