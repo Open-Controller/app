69a38000297b6c7cc10ae3b2f743a3fa73cad94d
PJTSearch <pjtsignups@gmail.com>
2021-03-06 14:13:11 -0800

    feat(SettingsView): Change house ref editing to expandable list

--- app/src/main/java/com/pjtsearch/opencontroller/ui/components/SettingsView.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/ui/components/SettingsView.kt
@@ -1,15 +1,20 @@
 package com.pjtsearch.opencontroller.ui.components
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import com.pjtsearch.opencontroller.components.ExpandableListItem
 import com.pjtsearch.opencontroller.settings.HouseRef
 import com.pjtsearch.opencontroller.settings.NetworkHouseRef
 import com.pjtsearch.opencontroller.settings.Settings
@@ -18,109 +23,63 @@ import com.pjtsearch.opencontroller.ui.theme.shapes
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+@ExperimentalAnimationApi
 @ExperimentalMaterialApi
 @Composable
 fun SettingsView(onError: (Throwable) -> Unit) {
     val ctx = LocalContext.current
     val settings = ctx.settingsDataStore.data.collectAsState(initial = Settings.getDefaultInstance())
     val scope = rememberCoroutineScope()
-    var editorShown by remember { mutableStateOf(false) }
-    var editingHouseRef: HouseRef? by remember { mutableStateOf(null) }
     Column {
         settings.value.houseRefsList.forEachIndexed { i, it ->
-            ListItem(
-                trailing = {
-                    IconButton(onClick = {
-                        scope.launch {
-                            ctx.settingsDataStore.updateData { settings ->
-                                settings.toBuilder().removeHouseRefs(i).build()
-                            }
+            ExpandableListItem(text = { Box(Modifier.fillMaxWidth()) {
+                Text(it.displayName, Modifier.align(Alignment.CenterStart))
+                IconButton(onClick = {
+                    scope.launch {
+                        ctx.settingsDataStore.updateData { settings ->
+                            settings.toBuilder().removeHouseRefs(i).build()
                         }
-                    }) {
-                        Icon(Icons.Outlined.Delete, "Delete this house")
                     }
                 },
-                modifier = Modifier.clickable { editingHouseRef = it; editorShown = true }
-            ) {
-                Text(it.displayName)
-            }
-        }
-        Button(onClick = {
-            editorShown = true
-            editingHouseRef = null
-        }) {
-            Text("Add house")
-        }
-        EditHouseRefDialog(editorShown, editingHouseRef, { editorShown = false }, { scope.launch {
-            ctx.settingsDataStore.updateData { settings ->
-                if (editingHouseRef != null) {
-                    val i = settings.toBuilder().houseRefsList.indexOf(editingHouseRef)
-                    settings.toBuilder().removeHouseRefs(i)
+                Modifier.align(Alignment.CenterEnd)) {
+                    Icon(Icons.Outlined.Delete, "Delete this house")
                 }
-                settings.toBuilder().addHouseRefs(it).build()
+            }}) {
+                EditHouseRef(it) {
+                    scope.launch { ctx.settingsDataStore.updateData { settings ->
+                        settings.toBuilder()
+                            .removeHouseRefs(i)
+                            .addHouseRefs(it).build()
+                    }}
+                }
             }
-        }})
+        }
     }
 }
 
 @Composable
-fun EditHouseRefDialog(shown: Boolean, houseRef: HouseRef?, onClose: () -> Unit, onSave: (HouseRef) -> Unit) {
-    var houseRefBuilder by mutableStateOf(HouseRef.getDefaultInstance())
-    LaunchedEffect(houseRef) {
-        if (houseRef != null) {
-            houseRefBuilder = houseRefBuilder.toBuilder().mergeFrom(houseRef).build()
-        }
-    }
-    var closing by mutableStateOf(false)
+fun EditHouseRef(houseRef: HouseRef, onSave: (HouseRef) -> Unit) {
+    var houseRefBuilder by remember(houseRef) { mutableStateOf(HouseRef.newBuilder(houseRef).build()) }
     val scope = rememberCoroutineScope()
 
-    if (shown) {
-        AlertDialog(
-            shape = shapes.medium,
-            onDismissRequest = { scope.launch {
-                closing = true; delay(200); onClose()
-            }},
-            title = {
-                Text(houseRef?.let { "Edit House" } ?: "Add House")
-            },
-            confirmButton = {
-                // TODO: Get rid of this hack to to have text boxes disabled before dialog
-                // see https://issuetracker.google.com/issues/180021390 and https://issuetracker.google.com/issues/180124293
-                Button(onClick = { scope.launch {
-                    onSave(houseRefBuilder); closing = true; delay(200); onClose()
-                }}) {
-                    Text("Save")
-                }
-            },
-            dismissButton = {
-                // TODO: Get rid of this hack to to have text boxes disabled before dialog
-                // see https://issuetracker.google.com/issues/180021390 and https://issuetracker.google.com/issues/180124293
-                Button(onClick = { scope.launch {
-                   closing = true; delay(200); onClose()
-                }}) {
-                    Text("Close")
-                }
-            },
-            text = {
-                Column {
-                    TextField(
-                        enabled = !closing,
-                        value = houseRefBuilder.displayName,
-                        onValueChange = {
-                            houseRefBuilder =
-                                houseRefBuilder.toBuilder().setDisplayName(it).build()
-                            println(houseRefBuilder.displayName)
-                        })
-                    TextField(
-                        enabled = !closing,
-                        value = houseRefBuilder.networkHouseRef.url,
-                        onValueChange = {
-                            houseRefBuilder = houseRefBuilder.toBuilder().setNetworkHouseRef(
-                                houseRefBuilder.networkHouseRef.toBuilder().setUrl(it)
-                            ).build()
-                        })
-                }
-            }
-        )
+    Column {
+        TextField(
+            value = houseRefBuilder.displayName,
+            onValueChange = {
+                houseRefBuilder =
+                    houseRefBuilder.toBuilder().setDisplayName(it).build()
+            })
+        TextField(
+            value = houseRefBuilder.networkHouseRef.url,
+            onValueChange = {
+                houseRefBuilder = houseRefBuilder.toBuilder().setNetworkHouseRef(
+                    houseRefBuilder.networkHouseRef.toBuilder().setUrl(it)
+                ).build()
+            })
+        Button(onClick = { scope.launch {
+            onSave(houseRefBuilder);
+        }}) {
+            Text("Save")
+        }
     }
 }
