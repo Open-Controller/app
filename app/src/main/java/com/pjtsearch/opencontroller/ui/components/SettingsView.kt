0c7afa81473cbe759868a70f985368e622998a9e
PJTSearch <pjtsignups@gmail.com>
2021-03-06 15:05:55 -0800

    feat(SettingsView): Add add house button

--- app/src/main/java/com/pjtsearch/opencontroller/ui/components/SettingsView.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/ui/components/SettingsView.kt
@@ -3,17 +3,19 @@ package com.pjtsearch.opencontroller.ui.components
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.pjtsearch.opencontroller.components.ExpandableListItem
 import com.pjtsearch.opencontroller.settings.HouseRef
 import com.pjtsearch.opencontroller.settings.NetworkHouseRef
@@ -54,6 +56,17 @@ fun SettingsView(onError: (Throwable) -> Unit) {
                 }
             }
         }
+        ExpandableListItem(text = { Row {
+            Icon(Icons.Outlined.Add, "Add a house")
+            Text("Add house")
+        }}) {
+            EditHouseRef(HouseRef.getDefaultInstance()) {
+                scope.launch { ctx.settingsDataStore.updateData { settings ->
+                    settings.toBuilder()
+                        .addHouseRefs(it).build()
+                }}
+            }
+        }
     }
 }
 
@@ -62,14 +75,16 @@ fun EditHouseRef(houseRef: HouseRef, onSave: (HouseRef) -> Unit) {
     var houseRefBuilder by remember(houseRef) { mutableStateOf(HouseRef.newBuilder(houseRef).build()) }
     val scope = rememberCoroutineScope()
 
-    Column {
+    Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
         TextField(
+            label = { Text("Name") },
             value = houseRefBuilder.displayName,
             onValueChange = {
                 houseRefBuilder =
                     houseRefBuilder.toBuilder().setDisplayName(it).build()
             })
         TextField(
+            label = { Text("URL") },
             value = houseRefBuilder.networkHouseRef.url,
             onValueChange = {
                 houseRefBuilder = houseRefBuilder.toBuilder().setNetworkHouseRef(
