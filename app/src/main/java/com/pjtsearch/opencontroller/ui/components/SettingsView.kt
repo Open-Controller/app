1d42e4e65b23f456f101972db0304135738ca91e
PJTSearch <pjtsignups@gmail.com>
2021-03-06 12:06:29 -0800

    feat(SettingsView): Add house ref list

--- app/src/main/java/com/pjtsearch/opencontroller/ui/components/SettingsView.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/ui/components/SettingsView.kt
@@ -1,23 +1,46 @@
 package com.pjtsearch.opencontroller.ui.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.pjtsearch.opencontroller.settings.HouseRef
 import com.pjtsearch.opencontroller.settings.NetworkHouseRef
 import com.pjtsearch.opencontroller.settings.Settings
 import com.pjtsearch.opencontroller.settingsDataStore
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 
+@ExperimentalMaterialApi
 @Composable
 fun SettingsView(onError: (Throwable) -> Unit) {
     val ctx = LocalContext.current
+    val settings = ctx.settingsDataStore.data.collectAsState(initial = Settings.getDefaultInstance())
+    val scope = rememberCoroutineScope()
     Column {
+        settings.value.houseRefsList.forEachIndexed { i, it ->
+            ListItem(trailing = { IconButton(onClick = {
+                scope.launch {
+                    ctx.settingsDataStore.updateData { settings ->
+                        settings.toBuilder().removeHouseRefs(i).build()
+                    }
+                }
+            }) {
+                Icon(Icons.Outlined.Delete, "Delete this house")
+            }}) {
+                Text(it.displayName)
+            }
+        }
         Button(onClick = {
-            thread{ runBlocking {
+            scope.launch {
                 ctx.settingsDataStore.updateData {
                     Settings.newBuilder()
                             .addHouseRefs(
@@ -29,18 +52,9 @@ fun SettingsView(onError: (Throwable) -> Unit) {
                             )
                             .build()
                 }
-            }}
+            }
         }) {
             Text("Set house")
-        }
-        Button(onClick = {
-            thread{ runBlocking {
-                ctx.settingsDataStore.updateData {
-                    Settings.newBuilder().build()
-                }
-            }}
-        }) {
-            Text("Clear houses")
         }
     }
 }
