99663a577b2dca1dcd4872d6a8eeb945b29f304e
PJTSearch <pjtsignups@gmail.com>
2021-03-07 18:44:59 -0800

    feat(Icons): Use extra icons

--- app/src/main/java/com/pjtsearch/opencontroller/extensions/Icons.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/extensions/Icons.kt
@@ -1,11 +1,38 @@
 package com.pjtsearch.opencontroller.extensions
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material.icons.outlined.*
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.pjtsearch.opencontroller_lib_proto.Icon
 
 var icons = hashMapOf(
-        Icon.STAR to Icons.Outlined.Star
+        Icon.STAR to Icons.Outlined.Star,
+        Icon.ARROW_UP to Icons.Outlined.KeyboardArrowUp,
+        Icon.ARROW_DOWN to Icons.Outlined.KeyboardArrowDown,
+        Icon.ARROW_LEFT to Icons.Outlined.KeyboardArrowLeft,
+        Icon.ARROW_RIGHT to Icons.Outlined.KeyboardArrowRight,
+        Icon.SELECT to Icons.Outlined.GpsFixed,
+        Icon.VOLUME_UP to Icons.Outlined.VolumeUp,
+        Icon.VOLUME_DOWN to Icons.Outlined.VolumeDown,
+        Icon.VOLUME_MUTE to Icons.Outlined.VolumeOff,
+        Icon.BACK to Icons.Outlined.Undo,
+        Icon.ON to Icons.Outlined.Power,
+        Icon.OFF to Icons.Outlined.PowerOff,
+        Icon.FORWARD to Icons.Outlined.FastForward,
+        Icon.REVERSE to Icons.Outlined.FastRewind,
+        Icon.INFO to Icons.Outlined.Info,
+        Icon.HOME to Icons.Outlined.Home,
+        Icon.CHANNEL_UP to Icons.Outlined.KeyboardArrowUp,
+        Icon.CHANNEL_DOWN to Icons.Outlined.KeyboardArrowDown,
+        Icon.CHANNEL to Icons.Outlined.Copyright,
+        Icon.REMOVE to Icons.Outlined.Delete,
+        Icon.STOP to Icons.Outlined.StopCircle,
+        Icon.SKIP_FORWARD to Icons.Outlined.ArrowLeft,
+        Icon.SKIP_REVERSE to Icons.Outlined.ArrowRight,
+        Icon.RECORD to Icons.Outlined.FiberManualRecord,
+        Icon.PAUSE to Icons.Outlined.Pause,
+        Icon.PLAY to Icons.Outlined.PlayArrow,
+        Icon.GUIDE to Icons.Outlined.List,
+        Icon.DVR to Icons.Outlined.FiberDvr
 )
 
