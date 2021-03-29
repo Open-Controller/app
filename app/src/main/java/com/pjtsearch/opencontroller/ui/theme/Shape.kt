bb116d58d191513116b7d3aec91cf7814dcc870c
PJTSearch <pjtsignups@gmail.com>
2021-03-28 21:28:11 -0700

    feat(Shape): Make medium shape a circle

--- app/src/main/java/com/pjtsearch/opencontroller/ui/theme/Shape.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/ui/theme/Shape.kt
@@ -1,11 +1,12 @@
 package com.pjtsearch.opencontroller.ui.theme
 
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val shapes = Shapes(
     small = RoundedCornerShape(7.dp),
-    medium = RoundedCornerShape(7.dp),
+    medium = CircleShape,
     large = RoundedCornerShape(25.dp)
 )
