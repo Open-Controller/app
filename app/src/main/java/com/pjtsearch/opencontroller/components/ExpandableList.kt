3549eceaefc8ce2688341d57920b808f0cf4194d
PJTSearch <pjtsignups@gmail.com>
2021-03-29 11:58:54 -0700

    feat(ExpandableListItem, RoomsMenu): Add ExpandableListItem icons, Room icons

--- app/src/main/java/com/pjtsearch/opencontroller/components/ExpandableList.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/components/ExpandableList.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material.ExperimentalMaterialApi
@@ -20,8 +21,9 @@ import com.pjtsearch.opencontroller.ui.theme.shapes
 @ExperimentalAnimationApi
 @Composable
 fun ExpandableListItem(
-        text: @Composable () -> Unit,
         modifier: Modifier = Modifier,
+        text: @Composable () -> Unit,
+        icon: @Composable () -> Unit = {},
         onOpen: () -> Unit = {},
         content: @Composable ColumnScope.() -> Unit
 ) {
@@ -31,7 +33,8 @@ fun ExpandableListItem(
                 modifier = Modifier
                         .clip(shape = shapes.small)
                         .toggleable(opened, onValueChange = { onOpen(); opened = it }),
-                text = text
+                text = text,
+                icon = icon
         )
         AnimatedVisibility(visible = opened, modifier = Modifier.padding(start = 30.dp)) {
             Column(content = content)
--- app/src/main/java/com/pjtsearch/opencontroller/ui/components/RoomsMenu.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/ui/components/RoomsMenu.kt
@@ -4,12 +4,18 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
 import androidx.compose.material.ListItem
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.pjtsearch.opencontroller.components.ExpandableListItem
+import com.pjtsearch.opencontroller.extensions.icons
 import com.pjtsearch.opencontroller_lib_proto.Controller
 import com.pjtsearch.opencontroller_lib_proto.ControllerOrBuilder
 import com.pjtsearch.opencontroller_lib_proto.HouseOrBuilder
@@ -21,9 +27,18 @@ fun RoomsMenu(house: HouseOrBuilder, onControllerClick: (Controller) -> Unit) =
     Column(modifier = Modifier.fillMaxHeight()) {
         house.roomsList
                 ?.map { room ->
-                    ExpandableListItem(
-                            modifier = Modifier.fillMaxWidth().padding(5.dp).padding(start = 20.dp),
-                            text = { Text(room.displayName) }) {
+                    ExpandableListItem(Modifier.fillMaxWidth().padding(5.dp),
+                        {Text(room.displayName)},
+                        {when (val icon = icons[room.icon] ?: throw Error("Could not find icon " + room.icon.toString())) {
+                            is Int -> Icon(
+                                painterResource(icon),
+                                room.displayName
+                            )
+                            is ImageVector -> Icon(
+                                icon,
+                                room.displayName
+                            )
+                        }}) {
                         room.controllersList.map { controller ->
                             ListItem(
                                     text = { Text(controller.displayName) },
