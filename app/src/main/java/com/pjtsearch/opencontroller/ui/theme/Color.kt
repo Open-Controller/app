63accf119fc5bdb86e42dd154536fc00b24adac0
PJTSearch <pjtsignups@gmail.com>
2021-03-28 21:30:19 -0700

    feat(Color): Change grey1000, remove unneeded colors

--- app/src/main/java/com/pjtsearch/opencontroller/ui/theme/Color.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.pjtsearch.opencontroller.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val teal200 = Color(0xFF03DAC5)
 val blueA100 = Color(0xFF82B1FF)
 val blueA400 = Color(0xFF2979FF)
-val blueA700 = Color(0xFF2962FF)
 val blueGrey50 = Color(0xFFECEFF1)
-val grey1000 = Color(0xFF121212)
+val grey1000 = Color(0xFF0C0C0C)
 val black = Color(0xFF000000)
