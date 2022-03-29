bf7aca031a26fb4dc2c04ff38432aa7a1f70a19d
PJTSearch <pjtsignups@gmail.com>
2022-03-29 10:41:06 -0700

    feat(ModifyHouseRef, IconPicker): Add icon picker dialog

---
+++ app/src/main/java/com/pjtsearch/opencontroller/ui/components/IconPicker.kt
@@ -0,0 +1,49 @@
+package com.pjtsearch.opencontroller.ui.components
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.pjtsearch.opencontroller.extensions.OpenControllerIcon
+import com.pjtsearch.opencontroller.extensions.icons
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun IconPicker(icon: String?, onPick: (icon: String?) -> Unit, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(3),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier
+    ) {
+        itemsIndexed(icons.keys.toList()) { _, it ->
+            val opacity by animateFloatAsState(targetValue = if (icon == it) 1f else 0f)
+            Button(
+                onClick = {
+                    if (icon == it) {
+                        onPick(null)
+                    } else {
+                        onPick(it)
+                    }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.secondary.copy(opacity),
+                    contentColor = if (icon == it) MaterialTheme.colorScheme.onSecondary else MaterialTheme.colorScheme.onSurface
+                )
+            ) {
+                OpenControllerIcon(it, it)
+            }
+        }
+    }
+}
--- app/src/main/java/com/pjtsearch/opencontroller/ui/components/ModifyHouseRef.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/ui/components/ModifyHouseRef.kt
@@ -2,36 +2,70 @@ package com.pjtsearch.opencontroller.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Image
 import androidx.compose.material.icons.outlined.Link
 import androidx.compose.material.icons.outlined.TextFields
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.pjtsearch.opencontroller.extensions.OpenControllerIcon
 import com.pjtsearch.opencontroller.settings.HouseRef
+
+sealed interface ChoosingIconState {
+    object Closed : ChoosingIconState
+    data class Opened(val currentIcon: String?) : ChoosingIconState
+}
 
 
 @Composable
 fun ModifyHouseRef(houseRef: HouseRef, onChange: (HouseRef) -> Unit) {
     var houseRefBuilder by remember(houseRef) { mutableStateOf(HouseRef.newBuilder(houseRef).build()) }
+    var choosingIconState by remember { mutableStateOf<ChoosingIconState>(ChoosingIconState.Closed) }
 
     LaunchedEffect(houseRefBuilder) {
         onChange(houseRefBuilder)
     }
 
     Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
-        TextField(
-            value = houseRefBuilder.icon,
-            leadingIcon = { Icon(Icons.Outlined.Image, "Icon") },
-            label = { Text("Icon") },
-            onValueChange = {
-                houseRefBuilder =
-                    houseRefBuilder.toBuilder().setIcon(it).build()
-            })
+        val currentIconState = choosingIconState
+        if (currentIconState is ChoosingIconState.Opened) {
+            AlertDialog(
+                title = { Text("Choose Icon") },
+                text = {
+                    IconPicker(currentIconState.currentIcon, onPick = {
+                        choosingIconState = ChoosingIconState.Opened(it)
+                    })
+                },
+                onDismissRequest = {
+                    choosingIconState = ChoosingIconState.Closed
+                },
+                confirmButton = {
+                    Button(onClick = {
+                        val chosen = currentIconState.currentIcon
+                        houseRefBuilder = chosen?.let {
+                            houseRefBuilder.toBuilder().setIcon(it).build()
+                        } ?: houseRefBuilder.toBuilder().clearIcon().build()
+                        choosingIconState = ChoosingIconState.Closed
+                    }) {
+                        Text(text = "Set")
+                    }
+                }
+            )
+        }
+        TextButton(
+            onClick = { choosingIconState = ChoosingIconState.Opened(houseRefBuilder.icon) },
+            modifier = Modifier.align(
+                Alignment.CenterHorizontally
+            )
+        ) {
+            if (houseRefBuilder.icon != "") {
+                OpenControllerIcon(icon = houseRefBuilder.icon, text = houseRefBuilder.icon, size = 2)
+            } else {
+                Text("Chose Icon")
+            }
+        }
         TextField(
             value = houseRefBuilder.displayName,
             leadingIcon = { Icon(Icons.Outlined.TextFields, "Name") },
