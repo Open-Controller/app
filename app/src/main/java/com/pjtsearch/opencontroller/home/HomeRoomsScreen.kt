9b0c93625e1dc97b65753fc41018eb405f43b462
PJTSearch <pjtsignups@gmail.com>
2022-03-29 11:29:25 -0700

    feat(HomeRoomsScreen): Add loading placeholder

--- app/src/main/java/com/pjtsearch/opencontroller/home/HomeRoomsScreen.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/home/HomeRoomsScreen.kt
@@ -6,9 +6,10 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -19,16 +20,25 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.insets.LocalWindowInsets
 import com.google.accompanist.insets.rememberInsetsPaddingValues
-import com.pjtsearch.opencontroller.executor.Controller
-import com.pjtsearch.opencontroller.executor.House
+import com.google.accompanist.placeholder.PlaceholderHighlight
+import com.google.accompanist.placeholder.fade
+import com.google.accompanist.placeholder.placeholder
+import com.google.accompanist.placeholder.shimmer
 import com.pjtsearch.opencontroller.components.ControlledExpandableListItem
 import com.pjtsearch.opencontroller.components.ExpandingBar
 import com.pjtsearch.opencontroller.components.SmallIconButton
+import com.pjtsearch.opencontroller.executor.Controller
+import com.pjtsearch.opencontroller.executor.House
 import com.pjtsearch.opencontroller.extensions.OpenControllerIcon
 
 @OptIn(ExperimentalAnimationApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun HomeRoomsScreen(house: House?, isLoading: Boolean, onSelectController: (Pair<String, String>) -> Unit, onExit: () -> Unit) {
+fun HomeRoomsScreen(
+    house: House?,
+    isLoading: Boolean,
+    onSelectController: (Pair<String, String>) -> Unit,
+    onExit: () -> Unit
+) {
     val decayAnimationSpec = rememberSplineBasedDecay<Float>()
     val scrollBehavior = remember(decayAnimationSpec) {
         TopAppBarDefaults.exitUntilCollapsedScrollBehavior(decayAnimationSpec)
@@ -86,7 +96,20 @@ fun HomeRoomsScreen(house: House?, isLoading: Boolean, onSelectController: (Pair
                             }
                         }
                     }
-                } ?: item {Text("Loading")}
+                } ?: items(5) {
+                    ControlledExpandableListItem(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(5.dp)
+                            .placeholder(
+                                visible = true,
+                                color = MaterialTheme.colorScheme.surfaceTint.copy(alpha = 0.3f),
+                                shape = CircleShape,
+                                highlight = PlaceholderHighlight.shimmer(MaterialTheme.colorScheme.surfaceVariant),
+                            ),
+                        { Text("Loading") },
+                        { Text("Loading") }) {}
+                }
             }
         }
     )
