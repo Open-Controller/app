6586f470bccb1e9d836463fee706c459480ad5ed
PJTSearch <pjtsignups@gmail.com>
2022-03-29 19:46:58 -0700

    feat(ListItem): Use onSecondaryContainer color overlay for selected, make bg secondaryContainer

--- app/src/main/java/com/pjtsearch/opencontroller/components/ListItem.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/components/ListItem.kt
@@ -1,45 +1,43 @@
 package com.pjtsearch.opencontroller.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.contentColorFor
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.ColorUtils
 
 @Composable
 fun ListItem(
     modifier: Modifier,
     shape: Shape = RoundedCornerShape(15.dp),
-    color: Color = MaterialTheme.colorScheme.primary,
+    color: Color = MaterialTheme.colorScheme.secondaryContainer,
     selected: Boolean = false,
     clickAndSemanticsModifier: Modifier,
     content: @Composable () -> Unit
 ) {
-    CompositionLocalProvider(
-        LocalContentColor provides contentColorFor(color.copy(alpha = if (selected) 0.3f else 0.15f))
+    Surface(
+        modifier = modifier
+            .clip(shape)
+            .then(clickAndSemanticsModifier),
+        shape = shape,
+        color = Color(
+            ColorUtils.blendARGB(
+                color.toArgb(),
+                MaterialTheme.colorScheme.onSecondaryContainer.toArgb(),
+                if (selected) 0.15f else 0f
+            )
+        )
     ) {
-        Box(
-            modifier
-                .background(
-                    color = color.copy(alpha = if (selected) 0.3f else 0.15f),
-                    shape = shape
-                )
-                .clip(shape)
-                .then(clickAndSemanticsModifier),
-            propagateMinConstraints = true
-        ) {
-            Box(Modifier.padding(15.dp)) {
-                content()
-            }
+        Box(Modifier.padding(15.dp)) {
+            content()
         }
     }
 }
