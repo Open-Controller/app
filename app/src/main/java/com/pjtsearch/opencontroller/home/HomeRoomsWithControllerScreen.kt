e1c0a72609b24f987d2d1be5e0714657351c2305
PJTSearch <pjtsignups@gmail.com>
2022-03-30 19:38:22 -0700

    feat(HomeRoomsWithControllerScreen): Add navigation button and layout styles

--- app/src/main/java/com/pjtsearch/opencontroller/home/HomeRoomsWithControllerScreen.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/home/HomeRoomsWithControllerScreen.kt
@@ -1,28 +1,20 @@
 package com.pjtsearch.opencontroller.home
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.*
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.placeholder.PlaceholderHighlight
-import com.google.accompanist.placeholder.placeholder
-import com.google.accompanist.placeholder.shimmer
-import com.pjtsearch.opencontroller.components.ControlledExpandableListItem
 import com.pjtsearch.opencontroller.executor.Widget
-import com.pjtsearch.opencontroller.extensions.OpenControllerIcon
 import com.pjtsearch.opencontroller.ui.components.ControllerView
 
 @OptIn(
@@ -38,34 +30,54 @@ fun HomeRoomsWithControllerScreen(
     onExit: () -> Unit,
     onError: (Throwable) -> Unit
 ) {
-//    TODO: add appbar, move rooms column to shared component
     Row(
         Modifier.padding(
-            top = WindowInsets.statusBars.only(WindowInsetsSides.Top).asPaddingValues()
-                .calculateTopPadding()
+            WindowInsets.systemBars.asPaddingValues()
         )
     ) {
-        RoomControllerPicker(
-            uiState.house?.rooms,
-            modifier = Modifier.weight(3f),
-            onSelectController = onSelectController
-        )
-
-        Column(
-            Modifier
-                .padding(
-                    bottom = WindowInsets.navigationBars
-                        .only(WindowInsetsSides.Bottom)
-                        .asPaddingValues()
-                        .calculateBottomPadding()
-                )
-                .weight(5f)
+        Surface(
+            modifier = Modifier
+                .weight(3f)
+                .padding(20.dp),
+            color = MaterialTheme.colorScheme.surface,
+            tonalElevation = 4.dp,
+            shape = RoundedCornerShape(25.dp)
         ) {
+            Column(Modifier.padding(10.dp)) {
+                IconButton(
+                    onClick = { onExit() },
+                    modifier = Modifier.padding(5.dp).background(MaterialTheme.colorScheme.secondaryContainer, CircleShape)
+                ) {
+                    Icon(Icons.Outlined.ArrowBack, "Exit Home")
+                }
+                RoomControllerPicker(
+                    uiState.house?.rooms,
+                    modifier = Modifier
+                        .fillMaxHeight(),
+                    onSelectController = onSelectController
+                )
+            }
+        }
+        Column(Modifier.weight(5f)) {
             if (uiState is HomeUiState.HasController) {
-                ControllerView(
-                    uiState.selectedController, onError = onError,
-                    menuState = uiState.controllerMenuState,
-                    onInteractMenu = onInteractWithControllerMenu
+                Scaffold(
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = { Text(uiState.selectedController.displayName) },
+                        )
+                    },
+                    content = { innerPadding ->
+                        Column(
+                            Modifier
+                                .padding(innerPadding)
+                        ) {
+                            ControllerView(
+                                uiState.selectedController, onError = onError,
+                                menuState = uiState.controllerMenuState,
+                                onInteractMenu = onInteractWithControllerMenu
+                            )
+                        }
+                    }
                 )
             } else {
                 Text(
