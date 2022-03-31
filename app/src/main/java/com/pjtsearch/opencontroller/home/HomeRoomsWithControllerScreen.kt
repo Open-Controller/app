4508fd9115b174c5817a529d3bfa6d291613132b
PJTSearch <pjtsignups@gmail.com>
2022-03-31 11:53:27 -0700

    feat(HomeRoomsWithControllerScreen): Add controller open and change transitions

--- app/src/main/java/com/pjtsearch/opencontroller/home/HomeRoomsWithControllerScreen.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/home/HomeRoomsWithControllerScreen.kt
@@ -1,6 +1,7 @@
 package com.pjtsearch.opencontroller.home
 
-import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -13,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.pjtsearch.opencontroller.executor.Widget
 import com.pjtsearch.opencontroller.ui.components.ControllerView
@@ -61,46 +63,60 @@ fun HomeRoomsWithControllerScreen(
             }
         }
         Column(Modifier.weight(5f)) {
-            if (uiState is HomeUiState.HasController) {
-                Scaffold(
-                    topBar = {
-                        CenterAlignedTopAppBar(
-                            title = { Text(uiState.selectedController.displayName) },
-                        )
-                    },
-                    content = { innerPadding ->
-                        Column(
-                            Modifier
-                                .padding(innerPadding)
-                        ) {
-                            ControllerView(
-                                uiState.selectedController, onError = onError,
-                                menuState = uiState.controllerMenuState,
-                                onInteractMenu = onInteractWithControllerMenu
+            Crossfade(targetState = uiState is HomeUiState.HasController) { hasController ->
+                when (hasController) {
+                    true -> {
+                        check(uiState is HomeUiState.HasController)
+                        AnimatedContent(targetState = uiState.selectedController, transitionSpec = {
+                            fadeIn(animationSpec = tween(250, delayMillis = 90)) +
+                                    slideIn(
+                                        initialOffset = { IntOffset(0, 70) },
+                                        animationSpec = tween(250, delayMillis = 90)
+                                    ) with
+                                    fadeOut(animationSpec = tween(90))
+                        }) { controller ->
+                            Scaffold(
+                                topBar = {
+                                    CenterAlignedTopAppBar(
+                                        title = { Text(controller.displayName) },
+                                    )
+                                },
+                                content = { innerPadding ->
+                                    Column(
+                                        Modifier
+                                            .padding(innerPadding)
+                                    ) {
+                                        ControllerView(
+                                            controller,
+                                            onError = onError,
+                                            menuState = uiState.controllerMenuState,
+                                            onInteractMenu = onInteractWithControllerMenu
+                                        )
+                                    }
+                                }
                             )
                         }
                     }
-                )
-            } else {
-                Box(
-                    Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.spacedBy(40.dp)
+                    false -> Box(
+                        Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
                     ) {
-                        Icon(
-                            Icons.TwoTone.SettingsRemote,
-                            "Controller Icon",
-                            modifier = Modifier.size(250.dp),
-                            tint = MaterialTheme.colorScheme.onBackground.copy(0.3f),
-                        )
-                        Text(
-                            text = "Choose a Controller",
-                            color = MaterialTheme.colorScheme.onBackground.copy(0.4f),
-                            style = MaterialTheme.typography.displayLarge
-                        )
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.spacedBy(40.dp)
+                        ) {
+                            Icon(
+                                Icons.TwoTone.SettingsRemote,
+                                "Controller Icon",
+                                modifier = Modifier.size(250.dp),
+                                tint = MaterialTheme.colorScheme.onBackground.copy(0.3f),
+                            )
+                            Text(
+                                text = "Choose a Controller",
+                                color = MaterialTheme.colorScheme.onBackground.copy(0.4f),
+                                style = MaterialTheme.typography.displayLarge
+                            )
+                        }
                     }
                 }
             }
