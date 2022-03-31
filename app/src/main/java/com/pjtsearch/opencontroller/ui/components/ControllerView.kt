1a5f5c843efff607112e329966f491ef3a380221
PJTSearch <pjtsignups@gmail.com>
2022-03-31 13:07:03 -0700

    feat(ControllerView): Add background behind controller menu

--- app/src/main/java/com/pjtsearch/opencontroller/ui/components/ControllerView.kt
+++ app/src/main/java/com/pjtsearch/opencontroller/ui/components/ControllerView.kt
@@ -1,8 +1,12 @@
 package com.pjtsearch.opencontroller.ui.components
 
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.*
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -31,20 +35,29 @@ fun ControllerView(
                 )
             }
         }
-        AnimatedVisibility(visible = menuState is ControllerMenuState.Open) {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(5.dp),
-                modifier = Modifier.weight(1f)
+        AnimatedVisibility(
+            visible = menuState is ControllerMenuState.Open,
+            enter = fadeIn() + expandVertically(),
+            exit = fadeOut() + shrinkVertically(),
+        ) {
+            Surface(
+                modifier = Modifier.weight(1f).padding(5.dp),
+                color = MaterialTheme.colorScheme.surface,
+                shape = RoundedCornerShape(25.dp),
+                tonalElevation = 1.dp
             ) {
-                menuState.items.map {
-                    Widget(
-                        it,
-                        Modifier
-                            .fillMaxWidth()
-                            .padding(bottom = 10.dp),
-                        { items -> onInteractMenu(true, items) },
-                        onError
-                    )
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(5.dp),
+                    modifier = Modifier.padding(10.dp)
+                ) {
+                    menuState.items.map {
+                        Widget(
+                            it,
+                            Modifier.fillMaxWidth(),
+                            { items -> onInteractMenu(true, items) },
+                            onError
+                        )
+                    }
                 }
             }
         }
